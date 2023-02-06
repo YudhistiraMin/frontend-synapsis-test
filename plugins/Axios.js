c0c48d3fa705928ecbeb0836edaab1931a250bf5
yudhistira <211232635@uii.ac.id>
2023-02-06 09:25:16 +0700

    remove qs axios

--- plugins/Axios.js
+++ plugins/Axios.js
@@ -1,9 +1,9 @@
-import qs from "qs"
+// import qs from "qs"
 
 export default function ({ $axios }) {
-  $axios.defaults.paramsSerializer = function(params) {
-    return qs.stringify(params)
-  }
+  // $axios.defaults.paramsSerializer = function(params) {
+  //   return qs.stringify(params)
+  // }
   
   $axios.onRequest(config => {
     config.headers.common['Authorization'] = `Bearer d418a957eedd13a42a6e8cb64ba2cf17a375763a0ce5cf2b34bd69d28aa5bcb7`
